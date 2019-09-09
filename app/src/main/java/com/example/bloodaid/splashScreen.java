dbdb61814e75d7217ab4769d26c4df704c1635ee
nahid0335 <nahid0335@gmail.com>
2019-09-09 23:35:09 +0600

    splashscreen created - tamzid

---
+++ app/src/main/java/com/example/bloodaid/splashScreen.java
@@ -0,0 +1,29 @@
+package com.example.bloodaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class splashScreen extends AppCompatActivity {
+
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+    }
+}
