cf19469a5bfbb09c84bcf6224bd4c916580863cc
nahid0335 <nahid0335@gmail.com>
2019-09-30 01:22:08 +0600

    Home for Admin-tamzid

---
+++ app/src/main/java/com/example/bloodaid/backand/AdminHome.java
@@ -0,0 +1,14 @@
+package com.example.bloodaid.backand;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.bloodaid.R;
+import android.os.Bundle;
+
+public class AdminHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+}
--- app/src/main/java/com/example/bloodaid/backand/AdminLoginActivity.java
+++ app/src/main/java/com/example/bloodaid/backand/AdminLoginActivity.java
@@ -3,13 +3,26 @@ package com.example.bloodaid.backand;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.bloodaid.R;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminLoginActivity extends AppCompatActivity {
-
+    Button AdminLoginbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+        AdminLoginbtn =findViewById(R.id.Adminlogin_btn);
+
+        AdminLoginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(AdminLoginActivity.this,AdminHome.class));
+            }
+        });
     }
 }
