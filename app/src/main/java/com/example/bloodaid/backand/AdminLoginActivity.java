80388222bc8247d095478ff2a6f2fbe1c48c227b
nahid0335 <nahid0335@gmail.com>
2019-09-29 19:33:25 +0600

    LoginUI for Admin-tamzid

---
+++ app/src/main/java/com/example/bloodaid/backand/AdminLoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.bloodaid.backand;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.bloodaid.R;
+
+import android.os.Bundle;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
