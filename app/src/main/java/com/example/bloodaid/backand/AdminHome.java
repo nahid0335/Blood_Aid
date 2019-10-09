5e63aec7fac5ba0b49bd554558e607af55b64d70
nahid0335 <nahid0335@gmail.com>
2019-10-09 13:10:51 +0600

    Home for Admin finish-tamzid

--- app/src/main/java/com/example/bloodaid/backand/AdminHome.java
+++ app/src/main/java/com/example/bloodaid/backand/AdminHome.java
@@ -1,8 +1,11 @@
 package com.example.bloodaid.backand;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bloodaid.ErrorToast;
 import com.example.bloodaid.R;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminHome extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class AdminHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+    }
+
+    public void donor_cardView(View view) {
+        ErrorToast.successToast(AdminHome.this, "Donor");
+    }
+
+    public void ambulence_cardView(View view) {
+        ErrorToast.successToast(AdminHome.this, "Ambulence");
+    }
+
+    public void hospital_cardView(View view) {
+        ErrorToast.successToast(AdminHome.this, "Hospital");
+    }
+
+    public void admin_cardView(View view) {
+        ErrorToast.successToast(AdminHome.this, "Admin");
+    }
+
+    public void report_cardView(View view) {
+        ErrorToast.successToast(AdminHome.this, "Report");
+    }
+
+    public void profile_cardView(View view) {
+        ErrorToast.successToast(AdminHome.this, "profile");
+    }
+
+    public void logout_button(View view) {
+        ErrorToast.successToast(AdminHome.this, "Logout");
     }
 }
