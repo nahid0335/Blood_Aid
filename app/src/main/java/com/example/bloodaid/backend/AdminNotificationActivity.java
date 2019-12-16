6a8f7f68cf8a7e17cd97afbb235d6ab4c953c957
nahid0335 <nahid0335@gmail.com>
2019-12-16 22:53:47 +0600

    admin finish without notification- Tamzid

---
+++ app/src/main/java/com/example/bloodaid/backend/AdminNotificationActivity.java
@@ -0,0 +1,16 @@
+package com.example.bloodaid.backend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bloodaid.R;
+
+public class AdminNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_notification);
+    }
+}
